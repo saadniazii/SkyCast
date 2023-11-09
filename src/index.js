979a459f7157807c627655d24a1541d8c9426dba
Saad Niazi <saadniazifed+1@gmail.com>
2023-11-09 11:00:39 +0500

    Fixed the onload function

--- src/index.js
+++ src/index.js
@@ -1,17 +1,28 @@
 import { fetchAPI } from "./modules/fetchLocation";
+import { renderDetails } from "./modules/renderDetails";
 import "./style.css";
 const getLocationBtn = document.querySelector(".getLocationBtn");
 
 getLocationBtn.addEventListener("click", fetchAPI);
 
-// fetchAPI();
+window.addEventListener("load", async () => {
+  const response = await fetch(
+    `http://api.weatherapi.com/v1/current.json?key=7e8dd149578b4420bff102321230811&q=London`
+  );
 
-// weather: response.current.condition.text
-// city, country name: response.location.name, response.location.country
-// response in Celsius response.current.temp_c
+  response
+    .json()
+    .then(function (response) {
+      renderDetails(response);
+    })
+    .catch((error) => {
+      console.log("error: ", error);
+    });
+});
+
 // response in F response.current.temp_f
-// feels like in C: response.current.feelslike_c
+
 // feels like in F: response.current.feelslike_f
-// wind in kph: response.current.wind_kph
+
 // wind in mph: response.current.wind_mph
 // humidity: response.current.humidity
